f608db42218861e470df495195417d90db1569ff
Pooja Bhagat <66077775+581-pooja@users.noreply.github.com>
2023-03-10 23:56:36 +0530

    Create

---
+++ LC March 2023/382. Linked List Random Node.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector <int> values;
+    Solution(ListNode* head) {
+        ListNode* temp = head;
+        int i = 0;
+        while(temp){
+            values.push_back(temp->val);
+            temp = temp->next;
+        }
+    }
+    
+    int getRandom() {
+        int n = values.size();
+        return values[rand()%n];
+    }
+};
+
+// TC: O(N) | SC: O(N)
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
+
+/*
+
+Intuition:
+Get the size of the linked list and according to that calculate random number and random_number % n will return random index
+
+Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+Implement the Solution class:
+    Solution(ListNode head) Initializes the object with the head of the singly-linked list head.
+    int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be equally likely to be chosen.
+
+Example 1:
+Input
+["Solution", "getRandom", "getRandom", "getRandom", "getRandom", "getRandom"]
+[[[1, 2, 3]], [], [], [], [], []]
+Output
+[null, 1, 3, 2, 2, 3]
+Explanation
+Solution solution = new Solution([1, 2, 3]);
+solution.getRandom(); // return 1
+solution.getRandom(); // return 3
+solution.getRandom(); // return 2
+solution.getRandom(); // return 2
+solution.getRandom(); // return 3
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+
+*/
